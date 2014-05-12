56ff0083397cecf26e6153535740e3586b58e419
Andrei <reflection.dm@gmail.com>
2014-05-12 22:56:45 +0400

    Add basic algorithm template for inversion calculation

--- Courser.Stanford.Tests/Week1Tests.cs
+++ Courser.Stanford.Tests/Week1Tests.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Coursera.Stanford.Implementations;
 
 namespace Courser.Stanford.Tests
 {
     [TestClass]
     public class Week1Tests
     {
-        [TestMethod]
-        public void InversionTest1()
+        private InversionCalculationImpl sut;
+
+        [TestInitialize]
+        public void SetUp()
         {
+            sut = new InversionCalculationImpl();
+        }
+
+        [TestMethod]
+        public void TrivialSmallInputCase()
+        {
+            var input = new long[] { 1, 3, 5, 2, 4, 6 };
+
+            var result = sut.Calc(input);
+
+            Assert.AreEqual(3, result);
         }
     }
 }
--- Coursera.Stanford.Implementations/InversionCalculationImpl.cs
+++ Coursera.Stanford.Implementations/InversionCalculationImpl.cs
@@ -8,5 +8,45 @@ namespace Coursera.Stanford.Implementations
 {
     public class InversionCalculationImpl
     {
+        public int Calc(long[] input)
+        {
+            var result = CountAndSort(input);
+            return result.Count;
+        }
+
+        internal IntermediateResult CountAndSort(long[] input)
+        {
+            var result = new IntermediateResult();
+            //trivial case
+            if (input.Length == 1)
+                result.Count = 0;
+            else
+            {
+                var parts = Split(input);
+                var leftResult = CountAndSort(parts.Item1);
+                var rightResult = CountAndSort(parts.Item2);
+                var split = CountSplitInv(input);
+
+                result.Count = leftResult.Count + rightResult.Count + split.Count;
+            }
+
+            return result;
+        }
+
+        private IntermediateResult CountSplitInv(long[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Tuple<long[], long[]> Split(long[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class IntermediateResult
+    {
+        public int Count { get; set; }
+        public long[] IntermediateArray { get; set; }
     }
 }
