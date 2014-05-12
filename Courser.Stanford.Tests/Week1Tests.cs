f28d877050f1e815ec4bfb3b0b39e4c4d9b58306
Andrei <reflection.dm@gmail.com>
2014-05-12 21:49:22 +0400

    Create basic setup for 1st week

---
+++ Courser.Stanford.Tests/Week1Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Courser.Stanford.Tests
+{
+    [TestClass]
+    public class Week1Tests
+    {
+        [TestMethod]
+        public void InversionTest1()
+        {
+        }
+    }
+}
