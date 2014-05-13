2d6e446fd53cde00bd84ec493d326d62798c1bbf
Andrei <reflection.dm@gmail.com>
2014-05-13 23:21:06 +0400

    Add read file method and corresponding tests.

---
+++ Reflectiondm.Utils.Tests/FileHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Reflectiondm.Utils.Tests
+{
+    [TestClass]
+    public class FileHelperTests
+    {
+        [TestMethod]
+        [TestCategory("Integration")]
+        public void GetArrayFromFile_Success()
+        {
+            var result = FileHelper.GetArrayFromFile("IntegerArray.txt");
+
+            Assert.AreEqual(54044, result[0]);
+            Assert.AreEqual(100000, result.Length);
+            Assert.AreEqual(91901, result[99999]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        [TestCategory("Integration")]
+        public void GetArrayFromFile_NoFile_ThrowsFileNotFound()
+        {
+            var result = FileHelper.GetArrayFromFile("NonExist.ext");
+        }
+    }
+}
---
+++ Reflectiondm.Utils/FileHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflectiondm.Utils
+{
+    public static class FileHelper
+    {
+        public static long[] GetArrayFromFile(string filePath)
+        {
+            var result = new List<long>();
+            var fullPath = Path.GetFullPath(filePath);
+
+            if (!File.Exists(fullPath))
+                throw new FileNotFoundException(fullPath);
+
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var number = Int64.Parse(line);
+                    result.Add(number);
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
